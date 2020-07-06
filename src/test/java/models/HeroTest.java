f3686d9ec7517686b2cf47a0240bd46d922a55a6
edithntinyari <emothegi@gmail.com>
2020-07-06 17:07:11 +0300

    added passed test

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -9,37 +9,37 @@ import static org.junit.Assert.assertEquals;
 public class HeroTest {
     @Test
     public void Hero_instantiatesCorrectly_true() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("mavel",45,"mavel","mavel");
         assertEquals(true, myHero instanceof Hero);
     }
     @Test
     public void getName_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
-        String expected = "Vick";
+        Hero myHero = new Hero("Bety",45,"mavel","mavel");
+        String expected = "bety";
         assertEquals(expected, myHero.getName());
     }
     @Test
     public void getAge_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("bety",45,"mavel","mavel");
         int expected = 65;
         assertEquals(expected, myHero.getAge());
     }
     @Test
     public void getWeakness_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
-        String expected = "madem";
+        Hero myHero = new Hero("bety",45,"mavel","mavel");
+        String expected = "mavel";
         assertEquals(expected, myHero.getWeakness());
     }
     @Test
     public void getPower_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
-        String expected = "madem";
+        Hero myHero = new Hero("bety",45,"mavel","mavel");
+        String expected = "mavel";
         assertEquals(expected, myHero.getPower());
     }
     @Test
     public void getId_forHeroesName_int() {
         Hero.clear();
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("bety",45,"mavel","mavel");
         assertEquals(1, myHero.getId());
     }
 
